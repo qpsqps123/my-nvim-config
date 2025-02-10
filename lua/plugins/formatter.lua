8c893e3117cdce6ad51a02ff4176b086febd4ab7
qpsqps123 <qpsqps321@gmail.com>
2025-02-10 23:32:10 +0900

    add formatting config

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -2,6 +2,10 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
+      html = { "prettier" },
+      css = { "prettier" },
+      json = { "prettier" },
+      markdown = { "prettier" },
       javascript = { "prettier" },
       typescript = { "prettier" },
       javascriptreact = { "prettier" },
