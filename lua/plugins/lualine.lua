b7f139b3f8872b109b4e88f376c5864de7e95b57
qpsqps123 <qpsqps321@gmail.com>
2025-02-20 17:08:22 +0900

    display file path to statusline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,6 +7,15 @@ return {
       options = {
         theme = "codedark",
       },
+      sections = {
+        lualine_c = {
+          {
+            "filename",
+            path = 1, -- 프로젝트 루트부터 상대 경로 표시
+            shorting_target = 40, -- 너무 길면 '...'으로 생략
+          },
+        },
+      },
     })
   end,
 }
